e60dcb1979c4f76dd6f81765a64c05bf3ce7ec9a
peterjxl <peterjxl@qq.com>
2023-07-03 21:50:21 +0800

    演示@ConfigurationProperties

--- src/main/java/com/peterjxl/boot/controller/HelloController.java
+++ src/main/java/com/peterjxl/boot/controller/HelloController.java
@@ -1,10 +1,22 @@
 package com.peterjxl.boot.controller;
 
+import com.peterjxl.boot.bean.Car;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // @RestController = @Controller + @ResponseBody
 public class HelloController {
+
+
+    @Autowired
+    Car car;
+
+    @RequestMapping("/car")
+    public Car car() {
+        return car;
+    }
+
 
     @RequestMapping("/hello")
     public String hello() {
