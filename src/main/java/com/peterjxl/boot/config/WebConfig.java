22f7e98f24d7dcd11725a5d3405c8446c56246af
peterjxl <peterjxl@qq.com>
2023-07-13 19:09:52 +0800

    矩阵变量

--- src/main/java/com/peterjxl/boot/config/WebConfig.java
+++ src/main/java/com/peterjxl/boot/config/WebConfig.java
@@ -3,14 +3,31 @@ package com.peterjxl.boot.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
 
 @Configuration(proxyBeanMethods = false)
-public class WebConfig {
+public class WebConfig  {
 
     @Bean
     public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
         HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
         filter.setMethodParam("_m");
         return filter;
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+        return new WebMvcConfigurer() {
+            //配置路径参数的分隔符
+            @Override
+            public void configurePathMatch(PathMatchConfigurer configurer) {
+                UrlPathHelper urlPathHelper = new UrlPathHelper();
+                //不移除；后面的内容。矩阵变量功能就可以生效
+                urlPathHelper.setRemoveSemicolonContent(false);
+                configurer.setUrlPathHelper(urlPathHelper);
+            }
+        };
     }
 }
